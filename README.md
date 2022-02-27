ccdc06b8af36768758275ced1f08e3ecf2b691cc
gbarc <g.barceloalvarez@gmail.com>
2022-02-27 23:16:58 +0100

    Versión 1.2  - Añadido de footer  - Añadido de la barra de búsqueda con funcionalidad  - Solución de errores  - Actualizacion por API

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Link al repositorio de Github de la práctica 2:
 https://github.com/GonzaloBarcelo/Inditex
 Link al Github pages:
 https://gonzalobarcelo.github.io/Inditex/
+
+
+# Ultima actualización a 27/02/2022
+
+Añadido de un buscador de páginas internas 
+Añadido lectura de una API financiera
+Añadida la actualizacion de etiquetas mediante javascript a través de la respuesta de la Api
+Actualizacion de la pagina mediante bootstrap (noticias, footer, ...)
+Añadida la actualizacion de la tabla de ultimos valores a traves de la respuesta de la Api
+Actualizacion de valores mediante botón con Cooldown de 30 segundos para respetar el máximo diario de la API KEY
--- exec.js
+++ exec.js
@@ -32,3 +32,86 @@ function search_in_page(){
 	}
 
 }
+
+async function getFromApi(){
+
+
+	return fetch("https://yfapi.net/v6/finance/quote?region=ES&lang=es&symbols=ITX.MC",{method:'GET',params: {modules: 'defaultKeyStatistics,assetProfile'},
+	  headers: {
+	  	Accept:'application/json',
+	   // 'x-api-key': 'KaQZvRWBaM8b2nTReAEuF2kU4ka9QK1T4Lx8EnB8';
+	   'x-api-key':'2hs7zfIo7haWNgjmXj6X25U7BYXVOrkja7iOWkyp'
+	  }})
+	.then(response => response.json())
+	.then(data => {
+		
+		console.log(data);
+		/*
+		console.log(data.quoteResponse.result[0]);
+		console.log(data.quoteResponse.result[0].regularMarketPrice);
+		*/
+		return 	data.quoteResponse.result[0];
+	})
+	
+	.catch(function (error) {
+		console.log("Se ha producido un error")
+		console.error(error);
+	});
+
+
+}
+
+function readFileAndUpdate(){
+
+}
+function updateData(){
+	var result=getFromApi().then(response => { return (response.regularMarketPrice)});
+	console.log(result);
+	document.write("<p>");
+	document.write(result.regularMarketPrice);
+	document.writeln("</p>");
+}
+function updateCotizaciones(){
+	try{
+		getFromApi()
+		.then(response => {
+
+			document.getElementById("card-actual").innerHTML=response.regularMarketPrice;
+			let text= response.regularMarketChangePercent;
+			let result = String(text).substr(0, 5);
+			document.getElementById("card-var").innerHTML=result;
+			document.getElementById("card-apert").innerHTML=response.regularMarketOpen;
+			
+			document.getElementById("btn-actualizar").disabled = "true";
+
+			var table = document.getElementById('table-values-cotizaciones');
+			var row = table.insertRow(1);
+ 			var cell1 = row.insertCell(0);
+ 			var cell2 = row.insertCell(1);
+ 			var cell3 = row.insertCell(2);
+ 			var cell4 = row.insertCell(3);
+ 			var cell5 = row.insertCell(4);
+ 			var cell6 = row.insertCell(5);
+ 			var cell7 = row.insertCell(6);
+ 			var cell8 = row.insertCell(7);
+ 			var cell9 = row.insertCell(8);
+ 			
+ 			
+ 			cell1.innerHTML = "19/02/2022";
+ 			cell2.innerHTML =response.regularMarketPrice;
+ 			cell3.innerHTML =result;
+ 			cell4.innerHTML = response.regularMarketDayHigh;
+ 			cell5.innerHTML = response.regularMarketDayLow;
+ 			cell6.innerHTML = response.regularMarketVolume;
+ 			cell7.innerHTML = response.regularMarketPreviousClose;
+ 			cell8.innerHTML = response.regularMarketOpen;
+ 			cell9.innerHTML = response.marketCap;
+
+    		setTimeout(function() {document.getElementById("btn-actualizar").disabled = false;}, 30000);
+
+		})
+	}
+	catch{
+
+	}
+}
